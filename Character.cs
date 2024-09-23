b50838bfee8012339c1c3290e1890fb91fe8d973
charlie-flynn <charlie.flynn.business@gmail.com>
2024-09-23 16:53:58 -0500

    Initial commit

---
+++ Character.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace BattleArena
+{
+    internal class Character
+    {
+        private string _name = "Character";
+        private float _health = 10.0f;
+        private float _maxHealth = 10.0f;
+        private float _attackPower = 1.0f;
+        private float _defensePower = 1.0f;
+
+        public float Health
+        {
+            // gets the health variable
+            get => _health;
+            // set the health to what it is as long as it's above 0
+            private set { _health = Math.Clamp(value, 0, _maxHealth); }
+        }
+        public float MaxHealth { get { return _maxHealth; } }
+        public float AttackPower { get { return _attackPower; } }
+        public float DefensePower { get { return _defensePower; } }
+        public string Name { get { return _name; } }
+
+        public Character(string name, float maxHealth, float attackPower, float defensePower)
+        {
+            _name = name;
+            _maxHealth = maxHealth;
+            _health = maxHealth;
+            _attackPower = attackPower;
+            _defensePower = defensePower;
+        }
+
+        public float Attack(Character target)
+        {
+            float damage = Math.Max(0, _attackPower - target.DefensePower);
+            Console.WriteLine(Name + " attacked " + target.Name + " for " + damage + " damage!");
+            target.TakeDamage(damage);
+
+            if (target.Health > 0)
+            {
+                Console.WriteLine(target.Name + "'s Health: " + target.Health + "/" + target.MaxHealth);
+            }
+            
+            return damage;
+        }
+
+        public void TakeDamage(float damage)
+        {
+            Health -= damage;
+            if (Health == 0)
+            {
+                Die();
+            }
+        }
+
+        public void Heal(float health)
+        {
+            Health += health;
+            Console.WriteLine(Name + " healed " + health + " health!");
+            Console.WriteLine(Name + "'s health: " + Health + "/" + MaxHealth);
+        }
+
+        private void Die()
+        {
+            Console.WriteLine(Name + " has died!");
+        }
+
+        public void PrintStats()
+        {
+            Console.WriteLine("Name:          " + Name);
+            Console.WriteLine("Health:        " + Health + "/" + MaxHealth);
+            Console.WriteLine("Attack Power:  " + AttackPower);
+            Console.WriteLine("Defense Power: " + DefensePower);
+        }
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleArena
+{
+    internal class Game
+    {
+        Character player;
+        Character enemy;
+        private bool _gameOver = false;
+
+        private int GetInput(string description, string option1, string option2)
+        {
+            ConsoleKeyInfo key;
+            int inputRecieved = 0;
+
+            while (inputRecieved != 1 && inputRecieved != 2)
+            {
+                // Print options
+                Console.Clear();
+                Console.WriteLine(description);
+                Console.WriteLine("1. " + option1);
+                Console.WriteLine("2. " + option2);
+                Console.Write("> ");
+
+                // get input from player
+                key = Console.ReadKey();
+
+                // if first option
+                if (key.KeyChar == '1')
+                {
+                    // set input recieved to 1
+                    inputRecieved = 1;
+                }
+                // otherwise if second option
+                else if (key.KeyChar == '2')
+                {
+                    // set input recieved to 2
+                    inputRecieved = 2;
+                }
+                // else neither
+                else
+                {
+                    // display error message (m,y favrite mmmm)
+                    Console.WriteLine("\nInvalid Input");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+            }
+
+            // return the input recieved after the loop
+            Console.WriteLine();
+            return inputRecieved;
+        }
+
+        private void Start()
+        {
+            player = new Character(name: "Player", maxHealth: 100, attackPower: 10, defensePower: 5);
+            enemy = new Character(name: "Enemy", maxHealth: 100, attackPower: 9, defensePower: 5);
+
+            player.PrintStats();
+            Console.WriteLine();
+            enemy.PrintStats();
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private void Update()
+        {
+            if (player.Health == 0)
+            {
+                _gameOver = true;
+                return;
+            }
+            else
+            {
+                int input = GetInput("Your turn, what will you do?", "Attack", "Heal");
+                if (input == 1)
+                {
+                    float damage = player.Attack(enemy);
+                }
+                else
+                {
+                    player.Heal(10);
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
+            
+
+            
+
+            if (enemy.Health == 0)
+            {
+                _gameOver = true;
+                return;
+            }
+            else
+            {
+                enemy.Attack(player);
+                Console.ReadKey();
+            }
+        }
+
+        private void End()
+        {
+            Console.Clear();
+            if (enemy.Health == 0)
+            {
+                Console.WriteLine("YOU WON!!! :D");
+            }
+            else
+            {
+                Console.WriteLine("YOU LOST!!! D:");
+            }
+            Console.ReadKey();
+        }
+        public void Run()
+        {
+            Start();
+            while (!_gameOver)
+            {
+                Update();
+            }
+            End();
+        }
+    }
+}
